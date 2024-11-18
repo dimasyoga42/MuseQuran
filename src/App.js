f320d4a847b496ddf059bec849aacfff150926d7
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-18 11:49:29 +0700

    add: react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </Router>
   );
 }
 
