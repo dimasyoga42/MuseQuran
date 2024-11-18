921947aa791c848967237a8db5cb429a0a965eb0
dimasyoga42 <dimasyoga9090@gmail.com>
2024-11-18 17:22:47 +0700

    prettier

--- src/components/update.jsx
+++ src/components/update.jsx
@@ -1,8 +1,8 @@
 const Update = () => {
-    return (
-        <div className="w-full min-h-screen flex justify-center justify-items-center items-center">
-            <h1 className="font-Poppins font-extrabold text-2xl">Comming Soon</h1>
-        </div>
-    )
+  return (
+    <div className='w-full min-h-screen flex justify-center justify-items-center items-center'>
+      <h1 className='font-Poppins font-extrabold text-2xl'>Comming Soon</h1>
+    </div>
+  )
 }
-export default Update;
+export default Update
